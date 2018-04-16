d2df0b9a1428516506619d0055d481cda41eb195
nkean <nathankean2@gmail.com>
2018-04-16 13:25:32 -0500

    Creates div, counter, and buttons

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 console.log('Javascript working');
 
+let clickCount = 0;
+
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('jQuery working')
+    console.log('jQuery working');
+    $('#generateButton').click(newDiv);
+}
+
+function newDiv() {
+    clickCount ++;
+    let div = '<div><p>' + clickCount + '</p></div>';
+    let swapButton = '<button class="swapButton">Swap</button>';
+    let deleteButton = '<button class="deletButton">Delete</button>';
+    $('body').append(div);
+    $('body div:last').append(swapButton + deleteButton);
 }
